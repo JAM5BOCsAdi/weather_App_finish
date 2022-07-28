d6185b90a7ef5395719d985a618f737f6e1d35c9
CsAdi <oradam86@gmail.com>
2022-07-28 12:39:36 +0200

    Finished the BASE: Debug Banner disable

--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -7,6 +7,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Weather App',
       theme: ThemeData.dark(),
       home: const LoadingScreen(),
