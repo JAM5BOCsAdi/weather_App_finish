7ba4179a4927e22b76905b84d07321c3461b3819
CsAdi <oradam86@gmail.com>
2022-07-28 11:30:46 +0200

    Weather App

---
+++ android/app/src/main/kotlin/com/weatherApp/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.weatherApp.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/my_app_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
