da68be1f370687ec0a8f64dd6c51b352e2729535
CsAdi <oradam86@gmail.com>
2022-08-01 14:21:57 +0200

    TRY - CATCH | WeatherAPI

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -17,39 +18,56 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   // Get the current location by accuracy
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print("------ getCurrentPosition ------");
-    print(position);
-/*
-    LocationPermission permission = await Geolocator.checkPermission();
-    print("------ CHECK PERMISSION ------");
-    print(permission);
-    permission = await Geolocator.requestPermission();
-    print("------ REQUEST PERMISSION ------");
-    print(permission);
-    bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
-    print("------ LOCATION SERVICE ------");
-    print(isLocationServiceEnabled);
-*/
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    // Decimal latitude and longitude coordinates for Szombathely (Hungary):
+    double lat = 47.23088;
+    double lon = 16.62155;
+    String appidCurrent = "9e1f0e2cfca262ce8ad574425d8f993b";
+    String url = 'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$appidCurrent';
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: RawMaterialButton(
-            // Size of the Button:
-            //constraints: BoxConstraints.tightFor(height: 100.0, width: 100.0),
-            fillColor: Colors.blue,
-            onPressed: () {
-              //Get the current location
-              getLocation();
-            },
-            child: const Text('Get Location'),
-          ),
+    getData();
+    return const SafeArea(
+      child: Scaffold(),
+    );
+/*
+    TRY - CATCH
+
+    String myMargin = "abc";
+    double? myMarginAsADouble;
+    try {
+      myMarginAsADouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+    }
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.all(myMarginAsADouble ?? 30.0),
+          // To RUN " Default: 30.0 " you need to add "?" to double? myMarginAsADouble
+          // IF the " String myMargin = "15"; ", the myMarginAsADouble will (run) be the value.
+          // IF the " String myMargin = "abc"; ", the 30.0 will (run) be the value.
+          color: Colors.red,
         ),
       ),
     );
+*/
   }
 }
